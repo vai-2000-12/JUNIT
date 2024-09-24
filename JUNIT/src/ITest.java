8efc06632516513d8e919df22f2fb6c04db5b058
Vaibhav soni <vaibhavsoni1712@gmail.com>
2024-09-24 21:12:29 +0530

    commit

--- JUNIT/src/ITest.java
+++ JUNIT/src/ITest.java
@@ -28,12 +28,14 @@ public class ITest {
     @Test
     public void testDivision(){
           I k = new I();
-          Assume.assumeFalse(0 == 0);
+          Assume.assumeFalse(0 == 0); //this test will be skipped since 0 == 0 will return true
           //Assume.assumeTrue(0 == 0)
           assertEquals(2, k.divide(10, 2));
         //   assertEquals(5, k.divide(10, 2));  This comments If I open It then This Will TOO pass in Testing But Delete the Ignore Annotation
     }
+ 
 
+    //This is to Print Some lines in Debug Console and same follows For the BeforeClass Annotation
     @AfterClass
     public static void print1(){
      System.out.println("All the Test Case Passed..!!👌😊");
