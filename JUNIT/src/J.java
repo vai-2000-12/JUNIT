8f60cc65b609fb8f3da92b05ba1628c9d84b693e
Vaibhav soni <vaibhavsoni1712@gmail.com>
2024-09-24 22:25:43 +0530

    commit

---
+++ JUNIT/src/J.java
@@ -0,0 +1,7 @@
+package src;
+
+public class J {
+  public  boolean isOdd(int number){
+     return number % 2 == 1;
+  }   
+}
---
+++ JUNIT/src/JTest.java
@@ -0,0 +1,63 @@
+package src;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+
+
+public class JTest {
+  @BeforeClass
+  public static void printStatement(){
+     System.out.println("Test Is Starting..😱");
+    }    
+    
+
+    @Test
+    public void testIsOdd(){
+        J nn = new J();
+        assertTrue("The Number is Not An Odd" , nn.isOdd(3));   
+    }
+    
+    @AfterClass
+    public static void printStatement1(){
+      System.out.println("All test case Passed !!🥳");
+  }
+}
+
+/*
+    🚫 Since here the Number is Even 
+    ⚠️  assertTrue("The Number is Not An Odd" , nn.isOdd(4));  
+    ⚠️ So it will print the Stack Trace and Message :
+ java.lang.AssertionError: The Number is Not An Odd 👈   
+        at org.junit.Assert.fail(Assert.java:89)
+        at org.junit.Assert.assertTrue(Assert.java:42)
+        at src.JTest.testIsOdd(JTest.java:19)
+        at java.base/jdk.internal.reflect.DirectMethodHandleAccessor.invoke(DirectMethodHandleAccessor.java:104)
+        at java.base/java.lang.reflect.Method.invoke(Method.java:578)
+        at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:59)
+        at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
+        at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:56)
+        at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
+        at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+        at org.junit.runners.BlockJUnit4ClassRunner$1.evaluate(BlockJUnit4ClassRunner.java:100)
+        at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:366)
+        at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:103)
+        at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:63)
+        at org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)
+        at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)
+        at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)
+        at org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)
+        at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)
+        at org.junit.internal.runners.statements.RunBefores.evaluate(RunBefores.java:26)
+        at org.junit.internal.runners.statements.RunAfters.evaluate(RunAfters.java:27)
+        at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+        at org.junit.runners.ParentRunner.run(ParentRunner.java:413)
+        at org.eclipse.jdt.internal.junit4.runner.JUnit4TestReference.run(JUnit4TestReference.java:93)
+        at org.eclipse.jdt.internal.junit.runner.TestExecution.run(TestExecution.java:40)
+        at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:530)
+        at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.runTests(RemoteTestRunner.java:758)
+        at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.run(RemoteTestRunner.java:453)
+        at org.eclipse.jdt.internal.junit.runner.RemoteTestRunner.main(RemoteTestRunner.java:211)
+
+*/
