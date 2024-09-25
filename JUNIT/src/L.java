756bc9984e835c0c659e85b2e4ed6f3780a3ba4c
Vaibhav soni <vaibhavsoni1712@gmail.com>
2024-09-25 17:02:31 +0530

    commit

--- JUNIT/src/L.java
+++ JUNIT/src/L.java
@@ -2,7 +2,9 @@ package src;
 
 public class L {
     public int[]reverseArray(int[]arr){
+
         int [] reverse = new int[arr.length];
+        
         for( int i = 0 ; i < arr.length ; i++){
              reverse[i] = arr[arr.length -1-i];
         }
---
+++ JUNIT/src/N.java
@@ -0,0 +1,8 @@
+package src;
+
+public class N {
+    public boolean isEven(int num){
+        return num %2 == 0;
+    }
+    
+}
